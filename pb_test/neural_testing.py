eefc59c3f77e88a50a6e5a863bed1b85fd6082cf
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2012-11-22 22:22:09 -0200

    testing somethings

--- pb_test/neural_testing.py
+++ pb_test/neural_testing.py
@@ -4,7 +4,36 @@ Created on 22/11/2012
 @author: arruda
 '''
 
+from pybrain.structure import FeedForwardNetwork, LinearLayer, SigmoidLayer, FullConnection
 
-
+def simple_ffn():
+    "make a simple feed forward network"
+    
+    n = FeedForwardNetwork()
+    
+    inLayer = LinearLayer(2)
+    hiddenLayer = SigmoidLayer(3)
+    outLayer = LinearLayer(1)
+    
+    n.addInputModule(inLayer)
+    n.addModule(hiddenLayer)
+    n.addOutputModule(outLayer)
+    
+    n.addConnection(FullConnection(inLayer,hiddenLayer))
+    n.addConnection(FullConnection(hiddenLayer,outLayer))
+    
+    n.sortModules()
+    
+#    print n
+    
+#    print n.activate([1, 2])
+    for k,v in n.connections.items():
+        for c in v:
+            print c.name, c.params 
+    
+    print n.params
+    
 if __name__ == '__main__':
-    pass
+    simple_ffn()
+    
+    
